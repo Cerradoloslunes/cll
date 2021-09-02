365f2dd931521e8a439c75aeede4b385b2789b9b
Joaquin Viretti <joaquin.viretti@gmail.com>
2021-09-02 20:54:32 -0300

    traigo nombre de video

--- main.js
+++ main.js
@@ -4,7 +4,15 @@ $.getJSON("https://api.rss2json.com/v1/api.json?rss_url=" + encodeURIComponent(r
    var link = data.items[0].link;
    var id = link.substr(link.indexOf("=") + 1);
    $("#youtube_video").attr("href", "https://youtube.com/watch?v=" + id);
+   var url = `https://www.youtube.com/watch?v=${id}`;
+
+   /* Obtener título */
+   $.getJSON('https://noembed.com/embed',
+      { format: 'json', url: url }, function (data) {
+         console.log(data.title);
+      });
 });
+
 
 window.onscroll = function () { scrollFunction() };
 
@@ -13,7 +21,7 @@ function scrollFunction() {
       document.getElementById("logo").style.width = "90px";
       document.getElementById("navbar").style.paddingTop = "10px";
       document.getElementById("navbar").style.backgroundColor = "white";
-   } 
+   }
    if (document.body.scrollTop < 80 && document.documentElement.scrollTop < 80) {
       document.getElementById("logo").style.width = "200px";
       document.getElementById("navbar").style.paddingTop = "50px";
