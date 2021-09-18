16d604d8aaa35a83dac8a93a309fb5a08c383f20
Joaquin Viretti <joaquin.viretti@gmail.com>
2021-09-17 22:32:34 -0300

    Agrego sección video

--- main.js
+++ main.js
@@ -35,7 +35,7 @@ const scrollFunction = () => {
    if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
       document.getElementById("logo").style.width = "90px";
       document.getElementById("navbar").style.paddingTop = "10px";
-      //document.getElementById("navbar").style.backgroundColor = "white";
+      document.getElementById("navbar").style.backgroundColor = "white";
    }
    if (document.body.scrollTop < 80 && document.documentElement.scrollTop < 80) {
       document.getElementById("logo").style.width = "290px";
